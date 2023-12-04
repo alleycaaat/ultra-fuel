aa55d40bd911c84fbef7b009385e28ba250d9455
alleycaaat <achulslander@gmail.com>
2023-12-04 14:59:22 -0600

    forgot default error path

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from 'react';
+
+//** import screens
+import DefaultLayout from './layouts/DefaultLayout';
+import Error from './screens/Error';
+
+//** import helpers
+import { mainLoader } from './util/loaders';
+
 import './styles.scss';
+
+const router = createBrowserRouter(
+	[
+		{
+			element: <DefaultLayout />,
+			errorElement: <Error />,
+			loader: mainLoader,
+		},
+		{
+			path: '*',
+			element: <Error />
+		}
+	]
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>,
 );
