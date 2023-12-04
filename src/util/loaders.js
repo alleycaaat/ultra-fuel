cbda245e92ff511df411c5d36ecf72bb72bf44ce
alleycaaat <achulslander@gmail.com>
2023-12-04 15:32:47 -0600

    loader to retrieve user data

--- src/util/loaders.js
+++ src/util/loaders.js
@@ -1,6 +1,12 @@
+import { getCurrentUser } from '../auth/appwrite-helpers';
 import { fetchData } from './helpers';
 
 export function mainLoader() {
     const user = fetchData('user')
     return { user };
+}
+
+export async function profileLoader() {
+    const data = await getCurrentUser()
+    return data
 }
