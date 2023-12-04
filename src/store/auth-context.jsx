923fb678e07452246acca26f20899d264da27584
alleycaaat <achulslander@gmail.com>
2023-12-04 15:33:29 -0600

    store user in context

--- src/store/auth-context.jsx
+++ src/store/auth-context.jsx
@@ -3,12 +3,29 @@
 import { createContext, useReducer } from 'react';
 import { authReducer } from './reducer';
 
-export const AuthProvider = createContext({});
+export const AuthProvider = createContext({
+    currUser: false,
+    authUser: (data) => { },
+});
 
 export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, []);
 
-    const value = {};
+    const authUser = async (data) => {
+        if (!state.currUser) {
+            dispatch({
+                type: 'AUTH_USER', payload: {
+                    userID: data.$id,
+                    user: data.name,
+                }
+            });
+        }
+    };
+
+    const value = {
+        currUser: state.currUser,
+        authUser: authUser,
+    };
 
     return <AuthProvider.Provider value={value}>{children}</AuthProvider.Provider>;
 };
