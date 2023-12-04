17fc06ce8e7dd566be56715a3352d754026c7a54
alleycaaat <achulslander@gmail.com>
2023-12-04 14:42:02 -0600

    loading screen

---
+++ src/loading/index.jsx
@@ -0,0 +1,13 @@
+import './loading.scss';
+
+export default function Loading() {
+    return (
+        <div id='wrapper'>
+            <span aria-live='polite' className='sr-only'>
+                <p>Content is loading</p>
+            </span>
+            <div id='loading-text'>LOADING</div>
+            <div id='loading-content'></div>
+        </div>
+    );
+}
