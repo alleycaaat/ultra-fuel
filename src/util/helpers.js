a6e307bde5ed0b61ea10595200ac17a4ffb06c80
alleycaaat <achulslander@gmail.com>
2023-12-04 16:14:37 -0600

    local storage functions and validator helpers

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,3 +1,24 @@
+// ** local storage functions
+
+import isEmail from 'validator/lib/isEmail';
+import isURL from 'validator/lib/isURL';
+
 export const fetchData = (key) => {
     return JSON.parse(localStorage.getItem(key));
+};
+
+export const setData = (key, value) => {
+    return localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const deleteItem = ({ key }) => {
+    return localStorage.removeItem(key);
+};
+
+export const validateEmail = (email) => {
+    return isEmail(email);
+};
+
+export const validateURL = (url) => {
+    return isURL(url);
 };
