cb8b762aac64fc29c0d001764a0b1c9d7ae49696
alleycaaat <achulslander@gmail.com>
2023-12-05 10:35:27 -0600

    import layouts

--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,54 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 
-//** import screens
-import DefaultLayout from './layouts/DefaultLayout';
-import Error from './screens/Error';
-
 //** import helpers
-import { mainLoader } from './util/loaders';
+import { mainLoader, profileLoader } from './util/loaders';
+
+//** import layouts
+import { WrappingLayout } from './components/layouts/WrappingLayout'; //main layout that everything appears in, wraps public and private layouts
+import { PublicLayout } from './components/layouts/PublicLayout';
+import { PrivateLayout } from './components/layouts/PrivateLayout';
+
+
+//** import screens
+import Error from './components/screens/Error';
+import Home from './components/screens/Home';
+import Dashboard from './components/screens/Private/Dashboard';
+
 
 import './styles.scss';
 
 const router = createBrowserRouter(
 	[
 		{
-			element: <DefaultLayout />,
+			element: <WrappingLayout />,
 			errorElement: <Error />,
 			loader: mainLoader,
+			children: [
+				{
+					element: <PublicLayout />, loader: mainLoader,
+					children: [
+						{
+							path: '/', element: <Home />,
+						}
+					]
+				},
+				{
+					element: <PrivateLayout />, loader: mainLoader,
+					children: [
+						{
+							path: '/dashboard', element: <Dashboard />, loader: profileLoader,
+						},
+					]
+				}
+			]
 		},
 		{
 			path: '*',
 			element: <Error />
 		}
 	]
-)
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
