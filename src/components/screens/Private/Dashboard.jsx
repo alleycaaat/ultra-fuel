f56cdb20a0963927d2bf9bed206a7a8c48681295
alleycaaat <achulslander@gmail.com>
2023-12-06 12:59:33 -0600

    fixed path

--- src/components/screens/Private/Dashboard.jsx
+++ src/components/screens/Private/Dashboard.jsx
@@ -1,11 +1,10 @@
 import { useLoaderData } from 'react-router-dom';
 
 const Dashboard = () => {
-	const { user } = useLoaderData();
-
+	const user = useLoaderData();
 	return (
 		<div>
-			<h1>Welcome back, {user}!</h1>
+			<h1>Welcome back, {user.name}!</h1>
 		</div>
 	);
 };
