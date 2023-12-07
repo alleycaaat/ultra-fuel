8cb089ae63820479ee1edeed080849526a760d1d
alleycaaat <achulslander@gmail.com>
2023-12-07 13:06:28 -0600

    time component

---
+++ src/components/elements/Forms/TimedRace.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import { Wrapper } from '../wrappers/form-wrapper';
+import { Input } from './input';
+import { Label } from './labels';
+
+const TimedRace = ({ data, setData, time }) => {
+    return (
+        <Wrapper>
+            <Label
+                text='Event Duration:'
+                label='time'
+            />
+            <Input
+                type='text'
+                id='time'
+                name='time'
+                autoComplete='off'
+                onChange={(e) => setData({
+                    ...data,
+                    time: e.target.value
+                })}
+                value={time}
+                required
+            />
+        </Wrapper>
+    );
+};
+
+export default TimedRace;
