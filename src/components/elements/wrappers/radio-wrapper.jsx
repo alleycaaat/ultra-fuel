58775b8a9fab207681bdc6f14bddd86a22db8d86
alleycaaat <achulslander@gmail.com>
2023-12-08 14:25:31 -0600

    functionality would be swell

--- src/components/elements/wrappers/radio-wrapper.jsx
+++ src/components/elements/wrappers/radio-wrapper.jsx
@@ -1,12 +1,31 @@
-const RadioWrapper = () => {
+/* eslint-disable react/prop-types */
+const RadioWrapper = ({ data, setData }) => {
     return (
         <div className='radio-wrap'>
             <fieldset>
                 <legend className='sr-only'>Select the race measurement:</legend>
-                <input type='radio' id='miles' name='measurement' value='email' />
+                <input
+                    type='radio'
+                    id='miles'
+                    name='measurement'
+                    value='miles'
+                    onClick={(e) => setData({
+                        ...data,
+                        format: e.target.value
+                    })}
+                />
                 <label htmlFor='miles'>mi</label>
 
-                <input type='radio' id='kilometers' name='measurement' value='email' />
+                <input
+                    type='radio'
+                    id='kilometers'
+                    name='measurement'
+                    value='kilometers'
+                    onClick={(e) => setData({
+                        ...data,
+                        format: e.target.value
+                    })}
+                />
                 <label htmlFor='kilometers'>km</label>
             </fieldset>
         </div>
