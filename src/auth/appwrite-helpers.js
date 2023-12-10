f539e3e05b84ab0f32a0a9a464e331705238a197
alleycaaat <achulslander@gmail.com>
2023-12-10 11:25:23 -0600

    create new event function

--- src/auth/appwrite-helpers.js
+++ src/auth/appwrite-helpers.js
@@ -1,23 +1,34 @@
-import { ID } from 'appwrite';
+import { ID, Permission, Role } from 'appwrite';
+import { account, databases } from '../config/appwrite';
+const DATABASE_ID = '656e36651e99327e908c';
+const EVENT_COLLECTION = '6574e211cca4eeb0c544';
 
-import { account } from '../config/appwrite';
-
-// ** get user's account data
+//** get user's account data
 export const getCurrentUser = async () => {
     return await account.get();
 };
 
-// ** start user session
+//** start user session
 export const signIn = async (email, password) => {
     try {
         return await account.createEmailSession(
             email, password
         );
     } catch (e) {
-        console.log('error',e)
+        console.log('error', e);
     }
 };
 
+//** end user session
+export const signOut = async () => {
+    try {
+        await account.deleteSession('current');
+    } catch (e) {
+        console.log('error logging out', e);
+    }
+};
+
+//** create new user account
 export const createAccount = async (name, email, password) => {
     try {
         // ** create user account
@@ -26,5 +37,25 @@ export const createAccount = async (name, email, password) => {
         return await signIn(email, password);
     } catch (e) {
         return console.log('create account error', e.code, e);
+    }
+};
+
+//** create new event document tied to user
+export const createUserEvent = async (event, userID) => {
+    try {
+        await databases.createDocument(
+            DATABASE_ID,
+            EVENT_COLLECTION,
+            ID.unique(),
+            event,
+            [
+                Permission.delete(Role.user(userID)),
+                Permission.update(Role.user(userID)),
+                Permission.read(Role.user(userID)),
+            ]
+        );
+        return 'success';
+    } catch (e) {
+        return 'failed';
     }
 };
