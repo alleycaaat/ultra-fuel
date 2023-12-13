7fd0bb723f818eb2e9b357bb1c1bae86a82eeb2d
alleycaaat <achulslander@gmail.com>
2023-12-13 14:16:36 -0600

    this shouldve been an element of its own, merp

---
+++ src/components/elements/Forms/MeasureRadio.jsx
@@ -0,0 +1,37 @@
+import RadioWrapper from '../wrappers/radio-wrapper';
+import { GenLabel } from './labels';
+
+// eslint-disable-next-line react/prop-types
+const MeasureRadio = ({ data, setData }) => {
+    return (
+        <RadioWrapper
+            label='Select what how the event is measured'
+        >
+            <input
+                type='radio'
+                id='miles'
+                name='measurement'
+                value='miles'
+                onClick={() => setData({
+                    ...data,
+                    measurement: 'mi'
+                })}
+            />
+            <GenLabel label='miles' text='mi' />
+
+            <input
+                type='radio'
+                id='kilometers'
+                name='measurement'
+                value='kilometers'
+                onClick={() => setData({
+                    ...data,
+                    measurement: 'km'
+                })}
+            />
+            <GenLabel label='kilometers' text='km' />
+        </RadioWrapper>
+    );
+};
+
+export default MeasureRadio;
