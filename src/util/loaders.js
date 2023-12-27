9d7b830fba55979ded347db372bdeb04a8c858d5
alleycaaat <achulslander@gmail.com>
2023-12-27 15:57:03 -0600

    formatting

--- src/util/loaders.js
+++ src/util/loaders.js
@@ -2,17 +2,17 @@ import { getCurrentUser, getUserEvents } from '../auth/appwrite-helpers';
 import { fetchData } from './helpers';
 
 export function mainLoader() {
-    const user = fetchData('user')
+    const user = fetchData('user');
     return { user };
 }
 
 export async function profileLoader() {
-    const data = await getCurrentUser()
-    return data
+    const data = await getCurrentUser();
+    return data;
 }
 
 export async function userEventsLoader() {
-    const user = await getCurrentUser()
+    const user = await getCurrentUser();
     const data = await getUserEvents(user.$id);
-    return data
+    return data;
 }
