437e7f8c64103cfd00de25177c34a8456f0a7294
alleycaaat <achulslander@gmail.com>
2023-12-28 16:19:10 -0600

    return data for past events

--- src/components/screens/Private/PastEvents.jsx
+++ src/components/screens/Private/PastEvents.jsx
@@ -1,7 +1,22 @@
+import { useLoaderData } from 'react-router-dom';
+
 const PastEvents = () => {
+	const events = useLoaderData();
+
+	const formatDate = (date) => {
+		return `${ date[0] }  ${ date[1] } ${ date[2] }`;
+	};
+
 	return (
-		<div>
+		<div className='past-events'>
 			<h2>Past Events</h2>
+			{(events?.map((evnt, i) => (
+				<div className='event' key={i}>
+					<h4>{evnt.eventname}</h4>
+					<p><span className='title'>Goal</span> {evnt.goal}</p>
+					<p><span className='title'>Date</span> {formatDate(evnt.eventdate)}</p>
+				</div>
+			)))}
 		</div>
 	);
 };
